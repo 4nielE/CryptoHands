9a1bb915fb7eb00e92b5d86d151e6be65040cc9b
anielespi <aniel.espinosa@protonmail.com>
2022-08-30 23:06:15 +0200

    first commit

---
+++ util/interact.js
@@ -0,0 +1,167 @@
+var account = null;
+var contract = null;
+var provider = null;
+const ABI = [{ "inputs": [{ "internalType": "string", "name": "_name", "type": "string" }, { "internalType": "string", "name": "_symbol", "type": "string" }, { "internalType": "string", "name": "_initBaseURI", "type": "string" }, { "internalType": "string", "name": "_initNotRevealedUri", "type": "string" }], "stateMutability": "nonpayable", "type": "constructor" }, { "anonymous": false, "inputs": [{ "indexed": false, "internalType": "uint256", "name": "_amountMinted", "type": "uint256" }], "name": "AmountMinted", "type": "event" }, { "anonymous": false, "inputs": [{ "indexed": true, "internalType": "address", "name": "owner", "type": "address" }, { "indexed": true, "internalType": "address", "name": "approved", "type": "address" }, { "indexed": true, "internalType": "uint256", "name": "tokenId", "type": "uint256" }], "name": "Approval", "type": "event" }, { "anonymous": false, "inputs": [{ "indexed": true, "internalType": "address", "name": "owner", "type": "address" }, { "indexed": true, "internalType": "address", "name": "operator", "type": "address" }, { "indexed": false, "internalType": "bool", "name": "approved", "type": "bool" }], "name": "ApprovalForAll", "type": "event" }, { "anonymous": false, "inputs": [{ "indexed": true, "internalType": "address", "name": "previousOwner", "type": "address" }, { "indexed": true, "internalType": "address", "name": "newOwner", "type": "address" }], "name": "OwnershipTransferred", "type": "event" }, { "anonymous": false, "inputs": [{ "indexed": true, "internalType": "address", "name": "from", "type": "address" }, { "indexed": true, "internalType": "address", "name": "to", "type": "address" }, { "indexed": true, "internalType": "uint256", "name": "tokenId", "type": "uint256" }], "name": "Transfer", "type": "event" }, { "inputs": [{ "internalType": "address", "name": "", "type": "address" }], "name": "addressMintedBalance", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "stateMutability": "view", "type": "function" }, { "inputs": [{ "internalType": "address", "name": "to", "type": "address" }, { "internalType": "uint256", "name": "tokenId", "type": "uint256" }], "name": "approve", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [{ "internalType": "address", "name": "owner", "type": "address" }], "name": "balanceOf", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "baseExtension", "outputs": [{ "internalType": "string", "name": "", "type": "string" }], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "baseURI", "outputs": [{ "internalType": "string", "name": "", "type": "string" }], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "cost", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "stateMutability": "view", "type": "function" }, { "inputs": [{ "internalType": "uint256", "name": "tokenId", "type": "uint256" }], "name": "getApproved", "outputs": [{ "internalType": "address", "name": "", "type": "address" }], "stateMutability": "view", "type": "function" }, { "inputs": [{ "internalType": "address", "name": "owner", "type": "address" }, { "internalType": "address", "name": "operator", "type": "address" }], "name": "isApprovedForAll", "outputs": [{ "internalType": "bool", "name": "", "type": "bool" }], "stateMutability": "view", "type": "function" }, { "inputs": [{ "internalType": "address", "name": "_user", "type": "address" }], "name": "isWhitelisted", "outputs": [{ "internalType": "bool", "name": "", "type": "bool" }], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "maxMintAmount", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "maxSupply", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "stateMutability": "view", "type": "function" }, { "inputs": [{ "internalType": "uint256", "name": "_mintAmount", "type": "uint256" }], "name": "mint", "outputs": [], "stateMutability": "payable", "type": "function" }, { "inputs": [], "name": "name", "outputs": [{ "internalType": "string", "name": "", "type": "string" }], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "nftPerAddressLimit", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "notRevealedUri", "outputs": [{ "internalType": "string", "name": "", "type": "string" }], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "onlyWhitelisted", "outputs": [{ "internalType": "bool", "name": "", "type": "bool" }], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "owner", "outputs": [{ "internalType": "address", "name": "", "type": "address" }], "stateMutability": "view", "type": "function" }, { "inputs": [{ "internalType": "uint256", "name": "tokenId", "type": "uint256" }], "name": "ownerOf", "outputs": [{ "internalType": "address", "name": "", "type": "address" }], "stateMutability": "view", "type": "function" }, { "inputs": [{ "internalType": "bool", "name": "_state", "type": "bool" }], "name": "pause", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [], "name": "paused", "outputs": [{ "internalType": "bool", "name": "", "type": "bool" }], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "renounceOwnership", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [], "name": "reveal", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [], "name": "revealed", "outputs": [{ "internalType": "bool", "name": "", "type": "bool" }], "stateMutability": "view", "type": "function" }, { "inputs": [{ "internalType": "address", "name": "from", "type": "address" }, { "internalType": "address", "name": "to", "type": "address" }, { "internalType": "uint256", "name": "tokenId", "type": "uint256" }], "name": "safeTransferFrom", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [{ "internalType": "address", "name": "from", "type": "address" }, { "internalType": "address", "name": "to", "type": "address" }, { "internalType": "uint256", "name": "tokenId", "type": "uint256" }, { "internalType": "bytes", "name": "data", "type": "bytes" }], "name": "safeTransferFrom", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [{ "internalType": "address", "name": "operator", "type": "address" }, { "internalType": "bool", "name": "approved", "type": "bool" }], "name": "setApprovalForAll", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [{ "internalType": "string", "name": "_newBaseExtension", "type": "string" }], "name": "setBaseExtension", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [{ "internalType": "string", "name": "_newBaseURI", "type": "string" }], "name": "setBaseURI", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [{ "internalType": "uint256", "name": "_newCost", "type": "uint256" }], "name": "setCost", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [{ "internalType": "uint256", "name": "_limit", "type": "uint256" }], "name": "setNftPerAddressLimit", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [{ "internalType": "string", "name": "_notRevealedURI", "type": "string" }], "name": "setNotRevealedURI", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [{ "internalType": "bool", "name": "_state", "type": "bool" }], "name": "setOnlyWhitelisted", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [{ "internalType": "uint256", "name": "_newmaxMintAmount", "type": "uint256" }], "name": "setmaxMintAmount", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [{ "internalType": "bytes4", "name": "interfaceId", "type": "bytes4" }], "name": "supportsInterface", "outputs": [{ "internalType": "bool", "name": "", "type": "bool" }], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "symbol", "outputs": [{ "internalType": "string", "name": "", "type": "string" }], "stateMutability": "view", "type": "function" }, { "inputs": [{ "internalType": "uint256", "name": "index", "type": "uint256" }], "name": "tokenByIndex", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "stateMutability": "view", "type": "function" }, { "inputs": [{ "internalType": "address", "name": "owner", "type": "address" }, { "internalType": "uint256", "name": "index", "type": "uint256" }], "name": "tokenOfOwnerByIndex", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "stateMutability": "view", "type": "function" }, { "inputs": [{ "internalType": "uint256", "name": "tokenId", "type": "uint256" }], "name": "tokenURI", "outputs": [{ "internalType": "string", "name": "", "type": "string" }], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "totalSupply", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "stateMutability": "view", "type": "function" }, { "inputs": [{ "internalType": "address", "name": "from", "type": "address" }, { "internalType": "address", "name": "to", "type": "address" }, { "internalType": "uint256", "name": "tokenId", "type": "uint256" }], "name": "transferFrom", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [{ "internalType": "address", "name": "newOwner", "type": "address" }], "name": "transferOwnership", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [{ "internalType": "address", "name": "_owner", "type": "address" }], "name": "walletOfOwner", "outputs": [{ "internalType": "uint256[]", "name": "", "type": "uint256[]" }], "stateMutability": "view", "type": "function" }, { "inputs": [{ "internalType": "address[]", "name": "_users", "type": "address[]" }], "name": "whitelistUsers", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "name": "whitelistedAddresses", "outputs": [{ "internalType": "address", "name": "", "type": "address" }], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "withdraw", "outputs": [], "stateMutability": "payable", "type": "function" }];
+
+const CONTRACT_ADDRESS = "0x090AaB9C0C178Dfc8df46fdE3696bF551da8f9AD";
+/*
+(async () => {
+    if (window.ethereum) {
+
+        // CONNECTING WALLET
+        await window.ethereum.send('eth_requestAccounts');
+        window.web3 = new Web3(window.ethereum);
+
+        var accounts = await web3.eth.getAccounts();
+        account = accounts[0];
+
+        document.getElementById("connect-wallet").onclick = async () => {
+            document.getElementById("connect-wallet").textContent = account.slice(0, 4) + "..." + account.slice(38);
+        }
+
+        // MINTING PART
+        contract = new web3.eth.Contract(ABI, CONTRACT_ADDRESS);
+
+        // Choosing the number of mint
+        const plus = document.querySelector(".plus"), minus = document.querySelector(".minus"), num = document.querySelector(".num");
+        let a = 1;
+        plus.addEventListener("click", () => {
+            if (a < 2) {
+                a++;
+                document.getElementById("mint-amount").textContent = a;
+            } else {
+                a = 2;
+                document.getElementById("mint-amount").textContent = a;
+            }
+        });
+        minus.addEventListener("click", () => {
+            if (a > 1) {
+                a--;
+                document.getElementById("mint-amount").textContent = a;
+            } else {
+                a = 1;
+                document.getElementById("mint-amount").textContent = a;
+            }
+        });
+
+        document.getElementById('mint-button').onclick = async () => {
+
+            // set up transaction parameters
+            const transactionParameters = {
+                to: CONTRACT_ADDRESS, // Required except during contract publications.
+                from: account, // must match user's active address.
+            };
+
+            // set up transaction parameter data according to UI
+            switch (a) {
+                case 1:
+                    transactionParameters.data = contract.methods.mint(1).encodeABI();
+                    break;
+                case 2:
+                    transactionParameters.data = contract.methods.mint(2).encodeABI();
+                    break;
+            }
+
+            await window.ethereum.request({
+                method: "eth_sendTransaction",
+                params: [transactionParameters],
+            });
+        }
+
+        // TOTAL SUPPLY RENDERING
+        var totalsupply = await contract.methods.totalSupply().call();
+        document.getElementById("total-supply").innerHTML = totalsupply;
+    }
+})();
+*/
+
+(async () => {
+    if (window.ethereum) {
+
+        // CONNECT WALLET ON CLICK
+        document.getElementById("connect-wallet").onclick = async () => {
+
+            // REQUESTING WALLET
+            await window.ethereum.request({ method: 'eth_requestAccounts' });
+            window.web3 = new Web3(window.ethereum);
+            var accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+            account = accounts[0];
+
+            provider = new ethers.providers.Web3Provider(window.ethereum, "goerli");
+            const signer = provider.getSigner(account);
+            /*provider.send("eth_requestAccounts", []).then(() => {
+                provider.listAccounts().then((accounts) => {
+                    const signer = provider.getSigner(accounts[0]);
+
+                    // 3.1 Create instance of pet smart contract 
+                    
+                });
+            });*/
+
+            contract = new ethers.Contract(CONTRACT_ADDRESS, ABI, signer);
+
+            // CONTRACT ESTABLISHMENT
+
+            document.getElementById("connect-wallet").textContent = account.slice(0, 4) + "..." + account.slice(38);
+
+            // TOTAL SUPPLY RENDERING
+            var totalsupply = await contract.totalSupply();
+            document.getElementById("total-supply").textContent = totalsupply;
+        }
+
+        // CHOOSE NUMBER OF MINT INCREMENT/DECREMENT
+        const plus = document.querySelector(".plus"), minus = document.querySelector(".minus")
+        let amount_mint = 1; // default on UI
+        let minimum_mint = 1;
+        let maximum_mint = 2;
+        const mint_price = 0.003;
+        plus.addEventListener("click", () => {
+            if (amount_mint < maximum_mint) {
+                amount_mint++;
+                document.getElementById("mint-amount").textContent = amount_mint;
+            } else {
+                amount_mint = maximum_mint;
+                document.getElementById("mint-amount").textContent = amount_mint;
+            }
+        });
+        minus.addEventListener("click", () => {
+            if (amount_mint > minimum_mint) {
+                amount_mint--;
+                document.getElementById("mint-amount").textContent = amount_mint;
+            } else {
+                amount_mint = minimum_mint;
+                document.getElementById("mint-amount").textContent = amount_mint;
+            }
+        });
+
+        // MINTING ON CLICK
+        document.getElementById('mint-button').onclick = async () => {
+
+            // set up transaction parameters
+            const transactionParameters = {
+                to: CONTRACT_ADDRESS, // Required except during contract publications.
+                from: account, // must match user's active address.
+                value: String(amount_mint * mint_price * 10 ** 15),
+                data: contract.mint(amount_mint).encodeABI,
+            };
+
+            // set up transaction parameter data according to UI: amount_mint
+            /*switch (amount_mint) {
+                case 1:
+                    transactionParameters.data = contract.mint(1).encodeABI;
+                    break;
+                case 2:
+                    transactionParameters.data = contract.mint(2).encodeABI;
+                    break;
+            }*/
+
+            const txHash = await window.ethereum.request({
+                method: "eth_sendTransaction",
+                params: [transactionParameters],
+            });
+
+            txHash.wait();
+            console.log("txHash: " + txHash);
+        }
+    }
+})();
